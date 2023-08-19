d717ec62e37c92e9ba96a73c806bf23f1de5f6a7
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-08-19 20:28:30 +0200

    index page finished

--- JS/jackets.js
+++ JS/jackets.js
@@ -2,8 +2,13 @@ const url = "https://api.noroff.dev/api/v1/rainy-days";
 
 const jacketContainer = document.querySelector(".product-section");
 
+jacketContainer.innerHTML = `<div class="loading-animation">
+<div class="loader"></div>
+</div>`;
+
 async function fetchJackets() {
     try {
+        await new Promise(resolve => setTimeout(resolve, 2000));//only to see how the animation is(remove this)
         const response = await fetch(url);
         const jackets = await response.json();
        
