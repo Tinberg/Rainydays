4cadf155211ecd6d51b3e2b0cc9a52ff09dacbc9
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-10-02 13:21:07 +0200

    Update jackets.js

--- JS/jackets.js
+++ JS/jackets.js
@@ -18,10 +18,12 @@ function hideLoading() {
 
 //this function determinate which page and how many jackets it will display
 function determinePage() {
-  if (window.location.pathname.includes("/index.html")) {
-    return "page1";
+  const path = window.location.pathname;
+  
+  if (path === "/" || path.includes("index.html")) {
+      return "page1";
   } else {
-    return null;
+      return null;
   }
 }
 
